df0963630cf028ff55b5b3aa9e8949dc70fddddb
Emirhan0325 <67663069+Emirhan0325@users.noreply.github.com>
2022-11-16 22:47:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace degisken
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //1 den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program
+            int sayı = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayı)
+            {
+                toplam += sayac;
+                sayac++; 
+
+            }
+            Console.WriteLine(toplam / sayı);
+
+            // "a" dan "z" ye kadar tüm harfleri console a yazdır
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            Console.WriteLine("****** Foreach ******");
+
+            string arabalar = "Bmw, Mercedes, Ferrari, Nissan";
+            foreach (var araba in arabalar)
+            {
+                Console.Write(araba);
+            }
+
+        }
+        
+    }
+}       
